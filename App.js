3de441318b39a1a227b7686a3ee84d714c21ae49
Tarek Almanabri <tarek.almanabri94@gmail.com>
2021-12-22 13:34:15 +0100

    add Modal component

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
-import { StyleSheet, View, FlatList } from 'react-native';
+import { StyleSheet, View, FlatList, Button } from 'react-native';
 import { useState } from 'react';
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
 
 export default function App() {
   const [courseGoals, setCourseGoals] = useState([])
+  const [addMode, setAddMode] = useState(false)
 
   const goalHandler = (goalTitle) => {
     /* the item becomes an object that contains a random id and value of the entered item  */
     setCourseGoals(currentGoals => [...courseGoals, {id: Math.random().toString(), value: goalTitle}])
+    setAddMode(false)
   }
 
   const removeGoalHandler = (goalId) => {
@@ -17,13 +19,22 @@ export default function App() {
     } )
   }
 
+  const addNewGoal = () => {
+    setAddMode(true)
+  }
+
+  const cancelGoalHandler = () => {
+    setAddMode(false)
+  }
+
   return (
     /*FlatList is better for the performance, scrollable list that will scroll through the items on the page,
      if we use the ScrollView component, it will scroll through all the items even the not rendered ones */
     <View style={styles.screen}>
-      <GoalInput addGoal={goalHandler}/>
+      <Button title='Add new goal' onPress={addNewGoal} />
+      <GoalInput visible={addMode} addGoal={goalHandler} onCancel={cancelGoalHandler} />
       <FlatList
-        keyExtractor={(item, index)=> item.id}
+        keyExtractor={(item)=> item.id}
         data={courseGoals}
         style={{paddingRight: 20}}
         renderItem={itemData => (
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,4 +1,4 @@
-import { Button, StyleSheet, TextInput, View } from 'react-native';
+import { Button, StyleSheet, TextInput, View, Modal } from 'react-native';
 import { useState } from 'react';
 
 const GoalInput = (props) => {
@@ -8,27 +8,48 @@ const GoalInput = (props) => {
     setGoal(text)
   }
 
+  const addGoalHandler = () => {
+    if(goal !== ''){
+      props.addGoal(goal)
+      setGoal('')}
+    else {
+      alert("You should enter a goal")
+    }
+  }
+
   return(
-    <View style={styles.inputContainer} >
-      <TextInput placeholder='Course Goal' style={styles.input} onChangeText={goalInputHandler}
-        value={goal} />
-      <Button title="Click Me" onPress={() => props.addGoal(goal)} />
-    </View>
+    <Modal visible={props.visible} animationType='slide' >
+      <View style={styles.inputContainer} >
+        <TextInput placeholder='Course Goal' style={styles.input} onChangeText={goalInputHandler}
+          value={goal} />
+        <View style={styles.buttonsWrapper}>
+          <Button title="Cancel" color='red' onPress={props.onCancel} />
+          <Button title="Add" onPress={addGoalHandler} />
+        </View>
+      </View>
+    </Modal>
   )
 }
 
 const styles = StyleSheet.create({
   inputContainer:{
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flex:1,
+    justifyContent: 'center',
     alignItems: 'center'
   },
   input: {
     width: '80%',
     borderBottomColor: 'black',
     borderBottomWidth: 1,
-    padding: 10
+    padding: 10,
+    marginBottom: 10
   },
+  buttonsWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '60%',
+    marginTop: 10
+  }
 });
 
 export default GoalInput
